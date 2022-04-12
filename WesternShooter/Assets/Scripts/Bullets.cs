9c31e62400ad8155a4d0d8c60d6a130057018ea4
ChimeCat <103204539+ChimeCat@users.noreply.github.com>
2022-04-12 10:12:12 -0700

    Update

--- WesternShooter/Assets/Scripts/Bullets.cs
+++ WesternShooter/Assets/Scripts/Bullets.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Author:[Goldstein, Isys];
+//Date:[April 12, 2022];
+//Script:[Making bullet to harm player];
 public class Bullets : MonoBehaviour
 {
+    [Header("Projectile Variables")]
+    public float speed;
+    public bool goingLeft;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,13 @@ public class Bullets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(goingLeft == true)
+        {
+            transform.position += speed * Vector3.left * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += speed * Vector3.right * Time.deltaTime;
+        }
     }
 }
